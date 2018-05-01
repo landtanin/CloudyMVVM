f514bdb9b1f4d0dc468606a08cac6140b8f85475
Tanin Rojanapiansatith <sp_land@hotmail.com>
2018-05-01 10:26:56 +0100

    Modify AddLocVC to conform with the reactified AddLocVM

--- Cloudy/View Controllers/Locations View Controller/Add Location View Controller/AddLocationViewController.swift
+++ Cloudy/View Controllers/Locations View Controller/Add Location View Controller/AddLocationViewController.swift
@@ -7,118 +7,114 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 protocol AddLocationViewControllerDelegate {
     func controller(_ controller: AddLocationViewController, didAddLocation location: Location)
 }
 
 class AddLocationViewController: UIViewController {
-
+    
     // MARK: - Properties
-
+    
     @IBOutlet var tableView: UITableView!
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
     
     var viewModel: AddLocationViewViewModel!
-
+    
     // MARK: -
-
+    
     var delegate: AddLocationViewControllerDelegate?
-
+    
+    private let disposeBag = DisposeBag()
+    
     // MARK: - View Life Cycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = "Add Location"
         
-        viewModel = AddLocationViewViewModel()
+        viewModel = AddLocationViewViewModel(query: searchBar.rx.text.orEmpty.asDriver())
         
-        viewModel.locationsDidChange = { [unowned self] (location) in
-            
-            self.tableView.reloadData()
-            
-        }
+        viewModel.locations
+            .drive(onNext: { [unowned self] (_) in
+                self.tableView.reloadData()
+            })
+            .disposed(by: disposeBag)
         
-        viewModel.queryingDidChange = { [unowned self] (querying) in
-            
-            if querying {
-                self.activityIndicatorView.startAnimating()
-            } else {
-                self.activityIndicatorView.stopAnimating()
-            }
-            
-        }
-    }
+        viewModel.querying.drive(activityIndicatorView.rx.isAnimating).disposed(by: disposeBag)
 
+        searchBar.rx.searchButtonClicked
+            .asDriver(onErrorJustReturn: ())
+            .drive(onNext: { [unowned self] in
+                self.searchBar.resignFirstResponder()
+            })
+            .disposed(by: disposeBag)
+        
+        searchBar.rx.cancelButtonClicked
+            .asDriver(onErrorJustReturn: ())
+            .drive(onNext: { [unowned self] in
+                self.searchBar.resignFirstResponder()
+            })
+        
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-
+        
         // Show Keyboard
         searchBar.becomeFirstResponder()
     }
-
+    
     // MARK: - View Methods
-
+    
     private func setupView() {
-
+        
     }
-
+    
     private func updateView() {
-
+        
     }
-
+    
 }
 
 extension AddLocationViewController: UITableViewDataSource {
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.numberOfLocations
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: LocationTableViewCell.reuseIdentifier, for: indexPath) as? LocationTableViewCell else { fatalError("Unexpected Table View Cell") }
-
+        
         if let viewModel = viewModel.viewModelForLocation(at: indexPath.row) {
             // Configure TableViewCell
             cell.configure(withViewModel: viewModel)
         }
-
+        
         return cell
     }
-
+    
 }
 
 extension AddLocationViewController: UITableViewDelegate {
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         guard let location = viewModel.location(at: indexPath.row) else {return}
-
+        
         // Notify Delegate
         delegate?.controller(self, didAddLocation: location)
-
+        
         // Pop View Controller From Navigation Stack
         navigationController?.popViewController(animated: true)
     }
-
+    
 }
 
 extension AddLocationViewController: UISearchBarDelegate {
-
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        // Hide Keyboard
-        searchBar.resignFirstResponder()
-
-        viewModel.query = searchBar.text ?? ""
-    }
-
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        // Hide Keyboard
-        searchBar.resignFirstResponder()
-
-        viewModel.query = searchBar.text ?? ""
-    }
-
+    
 }
